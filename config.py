d25543164d11aade87300470672964e7ad1af8e0
Arseny <shahnovich61@gmail.com>
2021-10-10 12:26:16 +0300

    simple DS bot

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = 'Токин бота'
+
+POST_ID = # ID поста для раздачи роли
+
+ROLES = {} # ID ролей и реакции им соответствующие
+
+EXCROLES = ()
+
+PREF = '--'   # префиксы для команд
+NULLPREF = ''
+
+MAX_ROLES_PER_USER = 2
